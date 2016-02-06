d8ce822eecdbd4fce8cf7b6267af696e1e0064f0
Max Shashkov <m@n8v.me>
2016-02-06 21:25:39 +0300

    Force debug mode if there's no PRODUCTION env variable

--- app.py
+++ app.py
@@ -21,4 +21,6 @@ def crash_page():
 	raise ValueError('Crash here', 'as planned')
 
 if __name__ == "__main__":
-	app.run(debug=True)
+	app.run(host='0.0.0.0', 
+		    port=int(os.environ.get('PORT', '8080')),
+		    debug=True)
