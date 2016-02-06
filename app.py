ee9f04242dbc4367f4a32399a8ccedbfdcddf07f
Max Shashkov <m@n8v.me>
2016-02-06 19:03:19 +0300

    Mongo server access through MongoLab & PyMongo

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
+import os
+
 from flask import Flask
+from flask.ext.pymongo import PyMongo
+
+import credentials
 
 app = Flask(__name__)
+app.config['MONGO_URI'] = os.environ['MONGOLAB_URI']
+
+mongo = PyMongo(app)
 
 @app.route('/')
 def index():
-	return 'OK!'
+	return mongo.server_info()
 
 if __name__ == "__main__":
-	app.run()
+	app.run(debug=True)
